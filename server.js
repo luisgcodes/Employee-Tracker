5446e3bcb0d9f7aad2f2cf4b3147d74a691bd3f0
luisgcodes <lcienfuegos95@gmail.com>
2022-05-12 19:18:16 -0700

    creating prompts

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-const connection = mysql.createConnection({
+// Creates the connection using your user and password from the mysql database
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Chickennugget1',
     database: 'employee_db',
     port: 3001
   });
+
+// Connectes 'con' to the database if successful, if not throw err
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected!');
+  startApp();
+});
+
+function startApp() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'toDo',
+      message: 'Pick an option!',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role'
+      ],
+    },
+  ]);
+};
+
+
+startApp();
