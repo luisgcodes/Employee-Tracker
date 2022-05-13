fb909b9f4ec0ba92886017d6ff955ec80c1ddb59
luisgcodes <lcienfuegos95@gmail.com>
2022-05-12 21:37:09 -0700

    seeding

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE employee_db;
+
+-- Departments
+INSERT INTO department (name) VALUES ("Sales");
+INSERT INTO department (name) VALUES ("Human Recources");
+INSERT INTO department (name) VALUES ("Accounting");
+INSERT INTO department (name) VALUES ("Marketing");
+
